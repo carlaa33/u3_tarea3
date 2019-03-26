8fed5b4cecd0007cd98f817587cef8ca99afff85
carlaa33 <carla.cutte@hotmail.com>
2019-03-25 20:02:50 -0700

    CRUD BRAND

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const wagner = require('wagner-core');
+const bodyParser = require('body-parser');
+
+//ponga disponible models
+require('./models/models')(wagner);
+
+
+//Importan producRouter
+const productRouter = require('./routers/product.router')(wagner);
+const userRouter = require('./routers/user.router')(wagner);
+const brandRouter = require('./routers/brand.router')(wagner);
+
+//confi servidor express
+let app =express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//le pondra la ruta localhost:3002
+app.use("/products",productRouter);
+app.use("/users",userRouter);
+app.use("/brands",brandRouter);
+
+
+module.exports = app;
